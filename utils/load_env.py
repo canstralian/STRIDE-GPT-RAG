6baf8c6b76663754a8b5c8a1960092c96c34a3ec
Esteban Cara de Sexo <8595080+canstralian@users.noreply.github.com>
2025-02-02 14:30:41 +1000

    Create load_env.py

---
+++ utils/load_env.py
@@ -0,0 +1,38 @@
+import os
+from dotenv import load_dotenv
+
+def load_env_variables():
+    if os.path.exists('.env'):
+        load_dotenv('.env')
+
+    github_api_key = os.getenv('GITHUB_API_KEY')
+    if github_api_key:
+        st.session_state['github_api_key'] = github_api_key
+
+    openai_api_key = os.getenv('OPENAI_API_KEY')
+    if openai_api_key:
+        st.session_state['openai_api_key'] = openai_api_key
+
+    anthropic_api_key = os.getenv('ANTHROPIC_API_KEY')
+    if anthropic_api_key:
+        st.session_state['anthropic_api_key'] = anthropic_api_key
+
+    azure_api_key = os.getenv('AZURE_API_KEY')
+    if azure_api_key:
+        st.session_state['azure_api_key'] = azure_api_key
+
+    azure_api_endpoint = os.getenv('AZURE_API_ENDPOINT')
+    if azure_api_endpoint:
+        st.session_state['azure_api_endpoint'] = azure_api_endpoint
+
+    azure_deployment_name = os.getenv('AZURE_DEPLOYMENT_NAME')
+    if azure_deployment_name:
+        st.session_state['azure_deployment_name'] = azure_deployment_name
+
+    google_api_key = os.getenv('GOOGLE_API_KEY')
+    if google_api_key:
+        st.session_state['google_api_key'] = google_api_key
+
+    mistral_api_key = os.getenv('MISTRAL_API_KEY')
+    if mistral_api_key:
+        st.session_state['mistral_api_key'] = mistral_api_key
